0bd0c4270fedd06803e736a93a2f0832af533d3f
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-03-10 12:38:11 +0100

    Created Method GET for HomeResource

--- src/main/java/controller/HomeResource.java
+++ src/main/java/controller/HomeResource.java
@@ -1,4 +1,21 @@
 package controller;
 
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/")
 public class HomeResource {
+
+    private final Template home;
+
+    public HomeResource(Template home) {
+        this.home = home;
+    }
+    @GET
+    public TemplateInstance getHome() {
+        return home.instance();
+    }
+
 }
