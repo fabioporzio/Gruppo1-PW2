cf30ad663079076ec2e5dece74cd269360b34c99
fabioporzio <fabio.porzio00@gmail.com>
2025-03-11 09:43:23 +0100

    Created LogoutController

---
+++ src/main/java/controller/LogoutController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import jakarta.ws.rs.CookieParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.NewCookie;
+import jakarta.ws.rs.core.Response;
+import logic.SessionManager;
+
+import java.net.URI;
+
+import static logic.SessionManager.NAME_COOKIE_SESSION;
+
+@Path("/logout")
+public class LogoutController {
+
+    private final SessionManager sessionManager;
+
+    public LogoutController(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    @POST
+    public Response processLogout(
+            @CookieParam(NAME_COOKIE_SESSION) String sessionId
+    ) {
+        sessionManager.removeSession(sessionId);
+
+        return Response.seeOther(URI.create("/"))
+                .cookie(new NewCookie.Builder(NAME_COOKIE_SESSION).value(null).build())
+                .build();
+    }
+}
