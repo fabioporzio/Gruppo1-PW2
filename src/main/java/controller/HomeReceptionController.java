0851b9adf60dd9ceae5d91b116cf46b13d774f01
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-03-11 17:39:00 +0100

    Implemented method GET for HomeReceptionController

--- src/main/java/controller/HomeReceptionController.java
+++ src/main/java/controller/HomeReceptionController.java
@@ -1,4 +1,22 @@
 package controller;
 
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/home-reception")
 public class HomeReceptionController {
+
+    private final Template homeReception;
+
+    public HomeReceptionController(Template homeReception) {
+        this.homeReception = homeReception;
+    }
+
+    @GET
+    public TemplateInstance getHomeReception() {
+        return homeReception.instance();
+    }
+
 }
