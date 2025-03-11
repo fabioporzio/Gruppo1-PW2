5f0776a6d626344bc444e53162eff7f1c44e36f6
fabioporzio <fabio.porzio00@gmail.com>
2025-03-11 09:41:28 +0100

    Created Employee object

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,79 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Employee {
+    String id;
+    String name;
+    String surname;
+    LocalDate dateOfBirth;
+    String department;
+    String email;
+    String password;
+
+    public Employee(String id, String name, String surname, LocalDate dateOfBirth, String department, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.dateOfBirth = dateOfBirth;
+        this.department = department;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
