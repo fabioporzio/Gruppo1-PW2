94c3323eebda298802ccb717532653b6c8832238
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-03-12 09:21:41 +0100

    Changed variable of visit class

--- src/main/java/model/Visit.java
+++ src/main/java/model/Visit.java
@@ -6,55 +6,85 @@ import java.time.LocalTime;
 public class Visit {
     private String id;
     private LocalDate date;
-    private LocalTime startTime;
-    private LocalTime endTime;
-    private String badge;
+    private LocalTime startingHour;
+    private LocalTime expectedEndingHour;
+    private LocalTime actualEndingTime;
+    private String guestId;
+    private String employeeId;
+    private String badgeCode;
 
-    public Visit(String id, LocalDate date, LocalTime startTime, LocalTime endTime, String badge) {
+    public Visit(String id, LocalDate date, LocalTime startingHour, LocalTime expectedEndingHour, LocalTime actualEndingTime, String guestId, String employeeId, String badgeCode) {
         this.id = id;
         this.date = date;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.badge = badge;
+        this.startingHour = startingHour;
+        this.expectedEndingHour = expectedEndingHour;
+        this.actualEndingTime = actualEndingTime;
+        this.guestId = guestId;
+        this.employeeId = employeeId;
+        this.badgeCode = badgeCode;
     }
 
     public String getId() {
         return id;
     }
 
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public LocalTime getStartTime() {
-        return startTime;
-    }
-
-    public LocalTime getEndTime() {
-        return endTime;
-    }
-
-    public String getBadge() {
-        return badge;
-    }
-
     public void setId(String id) {
         this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
     }
 
     public void setDate(LocalDate date) {
         this.date = date;
     }
 
-    public void setStartTime(LocalTime startTime) {
-        this.startTime = startTime;
+    public LocalTime getStartingHour() {
+        return startingHour;
     }
 
-    public void setEndTime(LocalTime endTime) {
-        this.endTime = endTime;
+    public void setStartingHour(LocalTime startingHour) {
+        this.startingHour = startingHour;
     }
 
-    public void setBadge(String badge) {
-        this.badge = badge;
+    public LocalTime getExpectedEndingHour() {
+        return expectedEndingHour;
+    }
+
+    public void setExpectedEndingHour(LocalTime expectedEndingHour) {
+        this.expectedEndingHour = expectedEndingHour;
+    }
+
+    public LocalTime getActualEndingTime() {
+        return actualEndingTime;
+    }
+
+    public void setActualEndingTime(LocalTime actualEndingTime) {
+        this.actualEndingTime = actualEndingTime;
+    }
+
+    public String getGuestId() {
+        return guestId;
+    }
+
+    public void setGuestId(String guestId) {
+        this.guestId = guestId;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getBadgeCode() {
+        return badgeCode;
+    }
+
+    public void setBadgeCode(String badgeCode) {
+        this.badgeCode = badgeCode;
     }
 }
