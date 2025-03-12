d0fab220095d171710ae01d6d2a814063c05aada
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-03-12 09:57:48 +0100

    Implemented session managment for HomeReceptionController

--- src/main/java/controller/HomeReceptionController.java
+++ src/main/java/controller/HomeReceptionController.java
@@ -2,21 +2,47 @@ package controller;
 
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
+import jakarta.ws.rs.CookieParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+import logic.SessionManager;
+import model.Employee;
+
+import java.net.URI;
+
+import static logic.SessionManager.NAME_COOKIE_SESSION;
 
 @Path("/home-reception")
 public class HomeReceptionController {
 
     private final Template homeReception;
+    private final SessionManager sessionManager;
 
-    public HomeReceptionController(Template homeReception) {
+    public HomeReceptionController(Template homeReception, SessionManager sessionManager) {
         this.homeReception = homeReception;
+        this.sessionManager = sessionManager;
     }
 
     @GET
-    public TemplateInstance getHomeReception() {
-        return homeReception.instance();
+    public Response getHomeReception(@CookieParam(NAME_COOKIE_SESSION) String sessionId) {
+        if (sessionId != null) {
+            Employee employee = sessionManager.getEmployeeFromSession(sessionId);
+
+            if (employee == null) {
+                return Response.seeOther(URI.create("/home")).build();
+            } else {
+                return Response.ok(homeReception.data("employee", employee)).build();
+            }
+        }
+        return Response.seeOther(URI.create("/")).build();
     }
+
+    /*@Path("/show-visits")
+    @GET
+    public TemplateInstance showVisits() {
+        return homeReception.data();
+    }*/
+
 
 }
