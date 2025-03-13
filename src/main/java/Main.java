e7246a5e9716a1a185f863c3b092d36bfb6b00a3
Marco Corradini <marco.corradini@itsincom.it>
2025-03-13 16:35:56 +0100

    added Main java Filtra button now named filtra button in homeReception.html

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+/* Main progect quarkus */
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;  
+
+@QuarkusMain
+public class Main implements QuarkusApplication {
+    public static void main(String[] args) {
+        Quarkus.run(args);
+    }
+
+    @Override
+    public int run(String... args) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+}
